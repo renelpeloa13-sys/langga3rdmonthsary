147686b864da2155291110ffd846b6a498fe511e
renelpeloa13-sys <renelpeloa13@gmail.com>
2025-09-08 05:21:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-const images = [img1.png", "img2.png", "img3.png"];
-const messages = [
-  "I didn't know three months could mean so much… until I spent them with you. Miss taka Everyday and gustohon ko man nga ipa feel simo nga special ka everyday, nga treat ka or lagaw ta sa sbng hindi ko pa kaya pero ma abot gid ang time nga ma ubra ta tann nga gusto ta langga ha ?",
-  "Every day with you feels like a hidden level in the game of life – magical, rare, and worth fighting for. Ma padayun lang kita langga always ah. Upod kita. and i hope hindi ka ma tak an mag upod sakon ma ubra sang journey ta.. malayo pa pero malayo na! ",
-  "You're not just my favorite person – you're my favorite *everything*. Happy 3rd Monthsary Langga."
-];
+document.addEventListener("DOMContentLoaded", () => {
+    const images = ["img1.png", "img2.png", "img3.png"];
+    const messages = [
+      "I didn't know three months could mean so much… until I spent them with you. Miss taka Everyday and gustohon ko man nga ipa feel simo na.",
+      "Every day with you feels like a hidden level in the game of life ~ magical, rare, and worth fighting for. Ma padayun lang kita langga ko.",
+      "You're not just my favorite person – you're my favorite *everything*. Happy 3rd Monthsary Langga."
+    ];
 
-let current = 0;
-const imgElement = document.getElementById("mysteryImage");
+    let current = 0;
+    const imgElement = document.getElementById("mysteryImage");
 
-imgElement.addEventListener("click", () => {
-  alert(messages[current]);
-  current++;
+    imgElement.addEventListener("click", () => {
+      alert(messages[current]);
+      current++;
 
-  if (current < images.length) {
-    imgElement.src = images[current];
-  } else {
-    imgElement.style.display = "none";
-    const msg = document.createElement("h2");
-    msg.innerHTML = "💖 That's all for now.I Love you always Langga ko 💖";
-    document.querySelector(".center-content").appendChild(msg);
-  }
+      if (current < images.length) {
+        imgElement.src = images[current];
+      } else {
+        imgElement.style.display = "none";
+        const msg = document.createElement("h2");
+        msg.innerHTML = "💖 That's all for now. I Love you always Langga ko 💖";
+        document.querySelector(".center-content").appendChild(msg);
+      }
+    });
 });
-
